c9ad5d86aeb3369064413c38f0e80283952c66bf
Jay <aabaaiaaa@gmail.com>
2015-09-28 00:52:04 +0100

    Started network generation script
    Script is close to generating the same network as the hand written test,
    but the servers are not connecting as in the original. Need to compare
    and investigate.

--- scripts/app.js
+++ scripts/app.js
@@ -103,6 +103,30 @@ app = new (function(){
 				data: "Find G quick!"
 			});
 			console.log("test 6 complete");
+		},
+		generateTestCaseA: function(){
+			generateServer("A", ["B"]);
+			generateServer("C", ["B", "G", "D"]);
+			generateServer("E", ["B", "D", "F"]);
+
+			var aSoftware = generatedServerMapping["A"].addSoftware(new Software({
+				name: "PING Tool",
+				parameters: "(destinationServer)"
+			}));
+			aSoftware.setSourceCode(function(args){
+				var testPacket = new Packet({
+					server: this.getServer()
+				});
+				testPacket.setType("TCP");
+				testPacket.setPayload(args.data);
+				testPacket.setDestinationServer(args.destinationServer);
+				testPacket.send();
+			});
+
+			aSoftware.run({
+				destinationServer: generatedServerMapping["F"],
+				data: "data for F"
+			});
 		}
 	};
 
@@ -111,5 +135,22 @@ app = new (function(){
 		var newServer = new Server(args);
 		servers.push(newServer);
 		return newServer;
+	};
+	var generatedServerMapping = {};
+	var generateServer = function(serverName, serversConnected){
+		serversConnected = serversConnected || [];
+		generatedServerMapping[serverName] = addServer({
+			name: serverName
+		});
+		var aNic = generatedServerMapping[serverName].addHardware(new Nic());
+		if(serversConnected.length > 0){
+			$.each(serversConnected, function(index, server){
+				generatedServerMapping[server] = generatedServerMapping[server] || addServer({
+					name: server
+				});
+				var xNic = generatedServerMapping[server].addHardware(new Nic());
+				xNic.setConnection(aNic);
+			});
+		}
 	};
 });
